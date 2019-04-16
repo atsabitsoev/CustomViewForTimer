40676f1cee7eb58de406fe563653e6af6399779b
Atsamaz <atsabitsoev>
2019-04-16 18:02:42 +0300

    commit

---
+++ CustomViewForTimer/PomodoroTimerView.swift
@@ -0,0 +1,115 @@
+//
+//  PomodoroTimerView.swift
+//  CustomViewForTimer
+//
+//  Created by Ацамаз Бицоев on 16/04/2019.
+//  Copyright © 2019 Ацамаз Бицоев. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class PomodoroTimerView: UIView {
+    
+    
+    @IBInspectable var mainColor: UIColor = UIColor.gray
+    @IBInspectable var additionalColor: UIColor = UIColor.darkGray
+    @IBInspectable var arcsRadMultiplier: CGFloat = 0.75
+    
+    @IBInspectable var shadowColor: UIColor = UIColor.black.withAlphaComponent(0.80)
+    let shadowOffset = CGSize(width: 0, height: 0)
+    @IBInspectable var shadowBlurRadius: CGFloat = 5
+    
+    lazy var width = bounds.width
+    lazy var height = bounds.height
+    lazy var diameter = min(width, height)
+    var radius: CGFloat { get { return diameter / 2 } }
+    
+    lazy var context = UIGraphicsGetCurrentContext()!
+    
+    
+    override func draw(_ rect: CGRect) {
+        
+        mainCircle()
+        arcs(radius: radius * arcsRadMultiplier)
+    }
+    
+    override func layoutSubviews() {
+        setLabTime()
+    }
+    
+    
+    private func mainCircle() {
+
+        let inset = (max(width, height) - diameter)/2
+        let isWidthBigger = width > height
+        let mainCircleX = isWidthBigger ? inset : 0
+        let mainCircleY = isWidthBigger ? 0 : inset
+        let mainCircleOrigin = CGPoint(x: mainCircleX, y: mainCircleY)
+        let mainRect = CGRect(origin: mainCircleOrigin, size: CGSize(width: diameter, height: diameter))
+        let mainCirclePath = UIBezierPath(ovalIn: mainRect)
+        mainColor.setFill()
+        mainCirclePath.fill()
+        
+        createShadow(mainRect)
+    }
+    
+    private func arcs(radius: CGFloat) {
+        
+        let center = CGPoint(x: width/2, y: height/2)
+        let startAngles: [CGFloat] = [.pi / 12,
+                                      .pi / 12 + .pi / 2,
+                                      .pi / 12 + .pi,
+                                      .pi / 12 + 3 * .pi / 2]
+        let endAngles: [CGFloat] = [5 * .pi / 12,
+                                    5 * .pi / 12 + .pi / 2,
+                                    5 * .pi / 12 + .pi,
+                                    5 * .pi / 12 + 3 * .pi / 2]
+        
+        //Drawing Arcs
+        for i in 0..<startAngles.count {
+            let arcsPath = UIBezierPath(arcCenter: center,
+                                        radius: radius / 2,
+                                        startAngle: startAngles[i],
+                                        endAngle: endAngles[i],
+                                        clockwise: true)
+            arcsPath.lineWidth = radius
+            additionalColor.setStroke()
+            arcsPath.stroke()
+        }
+    }
+    
+    private func createShadow(_ rect: CGRect) {
+        
+        let mainOrigin = CGPoint(x: self.frame.origin.x + rect.origin.x,
+                             y: self.frame.origin.y + rect.origin.y)
+        let shadowView = UIView(frame: CGRect(origin: mainOrigin,
+                                              size: rect.size))
+        shadowView.layer.shadowOffset = shadowOffset
+        shadowView.layer.shadowColor = shadowColor.cgColor
+        shadowView.layer.shadowRadius = shadowBlurRadius
+        shadowView.layer.shadowOpacity = 1
+        shadowView.backgroundColor = UIColor.white.withAlphaComponent(1)
+        shadowView.layer.cornerRadius = rect.width / 2
+        
+        self.superview!.insertSubview(shadowView, at: 0)
+    }
+    
+    private func setLabTime() {
+        
+        let labTime = UILabel()
+        labTime.bounds.size = CGSize(width: 100, height: 41)
+        labTime.center = CGPoint(x: self.bounds.width/2, y: self.bounds.height/2)
+        labTime.backgroundColor = UIColor.white
+        labTime.layer.cornerRadius = 8
+        labTime.clipsToBounds = true
+        labTime.layer.borderWidth = 1
+        labTime.layer.borderColor = UIColor.black.cgColor
+        
+        labTime.text = "14:49"
+        labTime.textAlignment = .center
+        
+        self.addSubview(labTime)
+    }
+
+}
